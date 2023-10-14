5bd86887103a638099f4de4dd4dc94af2dff58c0
jacob hoefer <jacob.hoefer@gmail.com>
2023-10-14 14:44:39 -0400

    got 3 of 5 functions working need to make create fucntions click

--- generate/index.js
+++ generate/index.js
@@ -3,22 +3,93 @@ const db = require('../db/connection');
 
 async function getAllDepartments() {
     try {
-      const [rows, fields] = await db.promise().query('SELECT * FROM department');
+        const [rows, fields] = await db.promise().query('SELECT * FROM department');
 
-      return rows;
-      
+        return console.table(rows);
+
     } catch (err) {
-      console.error('No departments found: ', err);
+        console.error('No departments found: ', err);
     }
-  };
-  
+};
+
 async function getAllRoles() {
     try {
-        const [rows, fields] = await db.execute('SELECT * FROM part');
+        const [rows, fields] = await db.promise().query('SELECT * FROM part');
+
         return rows;
     } catch (err) {
         console.error('no roles found: ', err)
     }
 };
-  
-  module.exports = { getAllDepartments, getAllRoles };
+
+async function getAllEmployees() {
+    try {
+        const [rows, fields] = await db.promise().query('SELECT * FROM employees');
+
+        return rows
+    } catch (err) {
+        console.error('No employees found: ', err);
+    }
+};
+
+async function getDepartments() {
+    try {
+        const [rows] = await db.promise().query('SELECT title FROM department');
+
+        return rows.map(department => department.title);
+    } catch (err) {
+        console.error('Failed to get departments: ', err);
+        throw err;
+    }
+};
+
+async function createNewDepartment(answers) {
+    try {
+        const [rows, fields] = await db.promise().query('INSERT INTO department (title) VALUES (?)', [answers.title]);
+
+        return console.table(rows);
+    } catch (err) {
+        console.error('Faied to create departemnt: ', err);
+    }
+};
+
+async function createNewRole(answers) {
+    try {
+        const [departmentRow] = await db.promise().query('SELECT id FROM department WHERE title = ?', [answers.choice]);
+
+        if (departmentRow.length === 0) {
+            console.error('No department found')
+            return;
+        }
+
+
+        const department_id = departmentRow[0].id;
+
+        console.log(department_id)
+
+        const [rows, fields] = await db.promise().query('INSERT INTO part (title, salery, department_id) VALUES (?, ?, ?)'[answers.title, answers.salery, department_id]);
+
+        return console.table(rows);
+    } catch (err) {
+        console.error('Failed to create Role: ', err);
+    }
+};
+
+async function getRoles() {
+    try {
+        const [rows] = db.promise().query('SELECT title FROM part');
+        return rows.map(part => part.title);
+    } catch (err) {
+        console.error("Failed to get roles", err);
+    }
+};
+
+module.exports = {
+    getAllDepartments,
+    getAllRoles,
+    getAllEmployees,
+    createNewDepartment,
+    getDepartments,
+    createNewRole,
+    getRoles
+};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const { getAllDepartments } = require('./generate');
+const { getAllDepartments, getAllRoles, createNewDepartment, getDepartments, createNewRole, getRoles } = require('./generate');
 
 // const mySql = require('mysql2/promise');
 // const db = require('./db/connection');
@@ -14,18 +14,129 @@ const prompt = [
     }
 ];
 
+const departmentQuestions = [
+    {
+        type: 'input',
+        message: 'What is the departments title?',
+        name: 'title'
+    }
+];
+
+const roleQuestions = [
+    {
+        type: 'input',
+        message: 'What is the roles titile?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'how much will you rake in on this job?',
+        name: 'salery'
+    },
+    {
+        type: 'list',
+        message: 'What department is this role in?',
+        name: 'choice',
+        choices: []
+    }
+];
+
+const employeeQuestions = [
+    {
+        type: 'input',
+        message: 'What is the employees 1st name?',
+        name: 'first_name'
+    },
+    {
+        type:'input',
+        message: 'what is the employees last name?',
+        name: 'last_name'
+    },
+    {
+        type:'list',
+        message:'what is the employees role?',
+        name: 'role',
+        choices: []
+    },
+]
+
+
 function init() {
     inquirer.prompt(prompt).then(async (answer) => {
-      switch (answer.choice) {
-        case 'View all Departments':
-          try {
-            const departments = await getAllDepartments();
-            console.log(departments);
-          } catch (error) {
-            console.error('Error:', error);
-          }
-          break;
-      }
+        switch (answer.choice) {
+            case 'View all Departments':
+                try {
+                    const departments = await getAllDepartments();
+                    console.log(departments);
+                } catch (err) {
+                    console.error('Failed to get departments: ', err);
+                }
+                break;
+        }
+        switch (answer.choice) {
+            case 'view all roles':
+                try {
+                    const roles = await getAllRoles();
+                    console.log(roles);
+                } catch (err) {
+                    console.error('Failed to get roles: ', err);
+                }
+                break;
+        }
+        switch (answer.choice) {
+            case 'view all employees':
+                try {
+                    const employees = await getAllEmployees();
+                    console.log(employees);
+                } catch (err) {
+                    console.error('Failed to get Employees: ', err);
+
+                }
+                break;
+        }
+        switch (answer.choice) {
+            case 'add a department':
+                try {
+                    const answers = await inquirer.prompt(departmentQuestions);
+                    const newDepartment = await createNewDepartment(answers);
+                    console.log(newDepartment)
+                } catch (err) {
+                    console.error(`Fialed to create department: ${newDepartment} `, err);
+                }
+                break;
+        }
+        switch (answer.choice) {
+            case 'add a role':
+                try {
+                    const departments = await getDepartments();
+                    roleQuestions[2].choices = departments;
+
+                    const answers = await inquirer.prompt(roleQuestions);
+
+                    const newRole = await createNewRole(answers);
+                    console.log(newRole);
+                } catch (err) {
+                    console.error(`failed to create Role: ${newRole} `, err);
+                }
+                break;
+        }
+        switch (answer.choice) {
+            case 'add an employee':
+                try {
+                    const roles = await getRoles();
+                    employeeQuestions[2].choices = roles;
+
+                    const answers = await inquirer.prompt(answer);
+                    
+                    const newEmployee = await createNewEmployee(answers);
+                    console.log(newEmployee);
+                } catch (err) {
+                    console.error(`Failed to add employee: ${newEmployee} `, err)
+                }
+                break;
+        }
     });
-  }
+}
+
+
 init();
